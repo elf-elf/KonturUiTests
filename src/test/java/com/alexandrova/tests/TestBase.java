e359388ee8d3f19613627f625bb8abbcbfd77777
elf-elf <vasenkinalena@gmail.com>
2023-03-11 11:49:37 +0400

    add new UI test

--- src/test/java/com/alexandrova/tests/TestBase.java
+++ src/test/java/com/alexandrova/tests/TestBase.java
@@ -22,7 +22,8 @@ public class TestBase {
         Configuration.baseUrl = "https://kontur.ru/career/";
         Configuration.browserVersion = "110.0.5481.178";
         Configuration.browserSize = "1920x1080";
-        Configuration.remote = config.server();
+       // Configuration.remote = config.server();
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC",true);
